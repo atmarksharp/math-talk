681cc66776928a0fd545c9652058f74c205673a7
南部 晃史 <nanbuakifumi@NAMBUMAC.local>
2012-01-08 16:15:05 +0900

    add ctrl+enter

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,17 +1,1 @@
-/*$(document).ready(
-	function(){
-	
-		$("#submit_btn").click(
-			function(){
-				$("#content").append('');
-				if($("input[name=mode]:checked").val()=="equation"){
-					var eq=$("#comment").val().replace(/¥/g,"\\");
-					$("#content").append('<div class="post_article"><img src="http://chart.apis.google.com/chart?cht=tx&chs=1x0&chf=bg,s,FFFFFF&chco=000000&chl='+encodeURIComponent(eq)+'" alt="equation" /></div>');
-				}else{
-					$("#content").append('<div class="post_article">'+$("#comment").val()+"</div>");
-				}
-				$(".post_article:hidden").fadeIn("1500");
-			}
-		)
-	}
-);*/
+
