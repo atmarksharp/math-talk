8f8250c1ca7a255ba6bdcc4c3ba1c6998893b11c
南部 晃史 <nanbuakifumi@NAMBUMAC.local>
2012-01-08 22:41:45 +0900

    ver 0.0 released

--- web.js
+++ web.js
@@ -8,8 +8,6 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
-// Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
@@ -27,8 +25,6 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-// Routes
-
 app.get('/', routes.index);
 
 
@@ -37,17 +33,12 @@ app.listen(port);
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-// ソケットを作る
 var socketIO = require('socket.io');
-// クライアントの接続を待つ(IPアドレスとポート番号を結びつけます)
 var io = socketIO.listen(app);
 
-// クライアントが接続してきたときの処理
 io.sockets.on('connection', function(socket) {
   console.log("connection");
-  // メッセージを受けたときの処理
   socket.on('message', function(data) {
-    // つながっているクライアント全員に送信
     console.log("message");
     io.sockets.emit('message', {value: data.value});
   });
